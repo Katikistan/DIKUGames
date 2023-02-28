243d3a12cabae505fe14112d7b7513f616b19039
student <olifiurlovic@gmail.com>
2023-02-28 10:12:40 +0100

    Added Player class and copied code snippet from assignment text into  game.cs

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -1,25 +1,22 @@
+using System.IO;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
 using DIKUArcade;
 using DIKUArcade.GUI;
-using DIKUArcade.Input;
-
-namespace Galaga
-{
-    public class Game : DIKUGame
-    {
-        public Game(WindowArgs windowArgs) : base(windowArgs) {
-            // TODO: Set key event handler (inherited window field of DIKUGame class)
-        }
-
-        //private void KeyHandler(KeyboardAction action, KeyboardKey key) {} // TODO: Outcomment
-
-        public override void Render()
-        {
-            throw new System.NotImplementedException("Galaga game has nothing to render yet.");
-        }
-
-        public override void Update()
-        {
-            throw new System.NotImplementedException("Galaga game has no entities to update yet.");
-        }
+namespace Galaga;
+public class Game : DIKUGame {
+    private Player player;
+    public Game(WindowArgs windowArgs) : base(windowArgs) {
+        player = new Player(
+            new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
+            new Image(Path.Combine("Assets", "Images", "Player.png")));
+    }
+    public override void Render() {
+        //TODO: Render Game Entities
+        throw new System.NotImplementedException("Galaga game has nothing to render yet.");
+    }
+    public override void Update() {
+        throw new System.NotImplementedException("Galaga game has no entities to update yet.");
     }
 }
