260c1808a988084754d390f944f92d89b936c756
student <bogi@protonmail.ch>
2023-02-28 18:30:49 +0100

    Added Enemy.cs

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -1,19 +1,32 @@
 using System.IO;
+using System.Collections.Generic;
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
 using DIKUArcade.Math;
 using DIKUArcade;
 using DIKUArcade.GUI;
+using DIKUArcade.Events;
 namespace Galaga;
 public class Game : DIKUGame {
     private Player player;
+    private EntityContainer<Enemy> enemies;
     public Game(WindowArgs windowArgs) : base(windowArgs) {
         player = new Player(
             new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
             new Image(Path.Combine("Assets", "Images", "Player.png")));
+        
+        List<Image> images = ImageStride.CreateStrides (4, Path.Combine("Assets", "Images", "BlueMonster.png"));
+        const int numEnemies = 8;
+        enemies = new EntityContainer<Enemy>(numEnemies);
+        for (int i = 0; i < numEnemies; i++) {
+            enemies.AddEntity(new Enemy(
+                new DynamicShape(new Vec2F(0.1f + (float)i * 0.1f, 0.9f), new Vec2F(0.1f, 0.1f)),
+                new ImageStride(80, images)));
+        }
     }
     public override void Render() {
         player.Render();
+        enemies.RenderEntities();
     }
     public override void Update() {
     }
