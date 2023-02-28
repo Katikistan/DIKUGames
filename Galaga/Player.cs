6eda14e45c03359974e49d862448e213bec8e8b9
student <olifiurlovic@gmail.com>
2023-02-28 16:12:40 +0100

    added all the methods and fields that have to be implemented

--- Galaga/Player.cs
+++ Galaga/Player.cs
@@ -2,12 +2,19 @@ using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
 namespace Galaga;
 public class Player {
-    // TODO: Add private fields
+    private float moveLeft = 0.0f;
+    private float moveRight = 0.0f;
+    private const float MOVEMENT_SPEED = 0.01f;
+    
+
     private Entity entity;
     private DynamicShape shape;
     public Player(DynamicShape shape, IBaseImage image) {
         entity = new Entity(shape, image);
         this.shape = shape;
+    }
+    private void UpdateDirection() {
+
     }
     public void Move() {
     // TODO: move the shape and guard against the window borders
@@ -18,6 +25,7 @@ public class Player {
     public void SetMoveRight(bool val) {
     // TODO:set moveRight appropriately and call UpdateDirection()
     }
+    
     public void Render() {
         entity.RenderEntity();    
     }
