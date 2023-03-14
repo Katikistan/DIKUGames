3ada4ee50330d6aadee5433551bb3c798584ef43
student <olifiurlovic@gmail.com>
2023-03-14 18:38:27 +0100

    added squadronline

--- Galaga/Squadron/ISquadron.cs
+++ Galaga/Squadron/ISquadron.cs
@@ -6,7 +6,6 @@ namespace Galaga.Squadron;
 public interface ISquadron {
     EntityContainer<Enemy> Enemies {get;}
     int MaxEnemies {get;}
-    void CreateEnemies (List<Image> enemyStride,
-        List<Image> alternativeEnemyStride);
+    void CreateEnemies (List<Image> enemyStride);
 }
 
