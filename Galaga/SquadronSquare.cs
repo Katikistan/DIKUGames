7652b9d0fdd5541763261a0767ead71067a21f60
student <olifiurlovic@gmail.com>
2023-03-14 17:11:16 +0100

    Added Squadron formations

---
+++ Galaga/SquadronSquare.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using DIKUArcade.Graphics;
+using DIKUArcade.Entities;
+
+namespace Galaga.Squadron;
+
+public class SquadronSquare : ISquadron {
+    public EntityContainer<Enemy> Enemies {get;}
+    public int MaxEnemies {get;}
+    public void CreateEnemies (List<Image> enemyStride,
+        List<Image> alternativeEnemyStride) {
+
+        }
+}
