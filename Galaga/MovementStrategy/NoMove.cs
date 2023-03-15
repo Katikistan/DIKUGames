c7da62331493ec60eb5051a0673b4671db967e20
student <olifiurlovic@gmail.com>
2023-03-15 13:46:14 +0100

    movement classes

--- Galaga/MovementStrategy/NoMove.cs
+++ Galaga/MovementStrategy/NoMove.cs
@@ -5,6 +5,7 @@
 // public class NoMove {
 //     private void MoveEnemy(Enemy enemy) {
 //         enemy.Shape.MoveX;
+            // brug speed og direction
 //     }
 //     public void MoveEnemies (EntityContainer<Enemy> enemies) {
 //         foreach (Enemy enemy in enemies) {
