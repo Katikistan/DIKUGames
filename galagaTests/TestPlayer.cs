82c5987f8d4c90db95fdf6cd6d8059d3c148e4ed
student <olifiurlovic@gmail.com>
2023-03-20 13:47:48 +0100

    Testplayer done

--- galagaTests/TestPlayer.cs
+++ galagaTests/TestPlayer.cs
@@ -9,45 +9,84 @@ public class TestPlayer {
     private Player player;
     private GameEvent EventMoveLeft;
     private GameEvent EventMoveRight;
+    private GameEvent EventNothing;
     [SetUp]
     public void Setup() {
         DIKUArcade.GUI.Window.CreateOpenGLContext();
+        GalagaBus.GetBus().InitializeEventBus(
+            new List<GameEventType> { GameEventType.PlayerEvent });
         player = new Player(
             new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
             new Image(Path.Combine("..","Galaga","Assets", "Images", "Player.png")));
         Vec2F playerPos = player.GetPosition();
+        GalagaBus.GetBus().Subscribe(GameEventType.PlayerEvent, player);
+        
         EventMoveLeft = (new GameEvent {
             EventType = GameEventType.PlayerEvent,
             Message = "MOVE LEFT"
         });
         EventMoveRight = (new GameEvent {
             EventType = GameEventType.PlayerEvent,
-            Message = "MOVE Right"
+            Message = "MOVE RIGHT"
+        });
+        EventNothing = (new GameEvent {
+            EventType = GameEventType.PlayerEvent,
+            Message = ""
         });
     }
     [Test]
     public void TestMoveLeft() {
         GalagaBus.GetBus().RegisterEvent(EventMoveLeft);
-        player.ProcessEvent(EventMoveLeft);
+        GalagaBus.GetBus().ProcessEventsSequentially();
         player.Move();
         Vec2F playerPos = player.GetPosition();
         Assert.AreEqual(playerPos.X, 0.44f);
     }
-
-
     public void TestMoveRight() {
-        Assert.Pass();
-    }
-    public void TestRealeseLeft() {
-        Assert.Pass();
-    }
-    public void TestRealeseRight() {
-        Assert.Pass();
+        GalagaBus.GetBus().RegisterEvent(EventMoveRight);
+        GalagaBus.GetBus().ProcessEventsSequentially();
+        player.Move();
+        Vec2F playerPos = player.GetPosition();
+        Assert.AreEqual(playerPos.X, 0.46f);
     }
     public void TestNotOutOfBoundsLeft() {
-        Assert.Pass();
+        player = new Player(
+            new DynamicShape(new Vec2F(0.0f, 0.1f), new Vec2F(0.1f, 0.1f)),
+            new Image(Path.Combine("..","Galaga","Assets", "Images", "Player.png")));
+        GalagaBus.GetBus().RegisterEvent(EventMoveLeft);
+        GalagaBus.GetBus().ProcessEventsSequentially();
+        player.Move();
+        Vec2F playerPos = player.GetPosition();
+        Assert.AreEqual(playerPos.X, 0.0f);
     }
     public void TestNotOutOfBoundsRight() {
-        Assert.Pass();
+        player = new Player(
+            new DynamicShape(new Vec2F(1.0f - 0.1f, 0.1f), new Vec2F(0.1f, 0.1f)),
+            new Image(Path.Combine("..","Galaga","Assets", "Images", "Player.png")));
+        GalagaBus.GetBus().RegisterEvent(EventMoveRight);
+        GalagaBus.GetBus().ProcessEventsSequentially();
+        player.Move();
+        Vec2F playerPos = player.GetPosition();
+        Assert.AreEqual(playerPos.X, 1.0f - 0.1f);
+    }
+    public void TestMoveLeftRight() {
+        GalagaBus.GetBus().RegisterEvent(EventMoveLeft);
+        GalagaBus.GetBus().RegisterEvent(EventMoveRight);
+        GalagaBus.GetBus().ProcessEventsSequentially();
+        player.Move();
+        player.Move();
+        Vec2F playerPos = player.GetPosition();
+        Assert.AreEqual(playerPos.X, 0.45f);
+    }
+    public void TestMoveWrongLeft() {
+        EventMoveLeft = (new GameEvent {
+            EventType = GameEventType.PlayerEvent,
+            Message = "move left"
+        });
+        GalagaBus.GetBus().RegisterEvent(EventMoveLeft);
+        GalagaBus.GetBus().ProcessEventsSequentially();
+        player.Move();
+        Vec2F playerPos = player.GetPosition();
+        Assert.AreEqual(0.45f, playerPos.X);
     }
 }
