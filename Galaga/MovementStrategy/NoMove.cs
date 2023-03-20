52f0321e2c7eb50eb767529c96e26b21222bd9d4
student <olifiurlovic@gmail.com>
2023-03-20 15:15:56 +0100

    removed empty line

--- Galaga/MovementStrategy/NoMove.cs
+++ Galaga/MovementStrategy/NoMove.cs
@@ -3,7 +3,6 @@ using DIKUArcade.Entities;
 namespace Galaga.MovementStrategy;
 public class NoMove : IMovementStrategy {
     public void MoveEnemy(Enemy enemy) {
-
     }
     public void MoveEnemies (EntityContainer<Enemy> enemies) {
         foreach (Enemy enemy in enemies) {
