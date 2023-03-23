2eb9f60596a5fe3850ae86b44910e8129ca0cc3b
Katikistan <112586661+Katikistan@users.noreply.github.com>
2023-03-23 15:27:20 +0100

    formatted document

--- Galaga/Squadron/SquadronLine.cs
+++ Galaga/Squadron/SquadronLine.cs
@@ -6,23 +6,27 @@ using System.Collections.Generic;
 namespace Galaga.Squadron;
 
 public class SquadronLine : ISquadron {
-    public EntityContainer<Enemy> Enemies {get;}
-    public int MaxEnemies {get;}
+    public EntityContainer<Enemy> Enemies {
+        get;
+    }
+    public int MaxEnemies {
+        get;
+    }
 
-    public SquadronLine () {
+    public SquadronLine() {
         MaxEnemies = 6;
         Enemies = new EntityContainer<Enemy>(MaxEnemies);
     }
-    public void CreateEnemies (List<Image> enemyStride, List<Image> alternativeEnemyStride) {
+    public void CreateEnemies(List<Image> enemyStride, List<Image> alternativeEnemyStride) {
         ImageStride blueMonster = new ImageStride(80, enemyStride);
-        for (int i = 0; i < MaxEnemies/2; i++) {
+        for (int i = 0; i < MaxEnemies / 2; i++) {
             Enemies.AddEntity(new Enemy(
-                new DynamicShape(new Vec2F(0.1f + (float)i * 0.1f, 0.9f), new Vec2F(0.1f, 0.1f)),
+                new DynamicShape(new Vec2F(0.1f + (float) i * 0.1f, 0.9f), new Vec2F(0.1f, 0.1f)),
                 blueMonster));
         }
-        for (int i = 0; i < MaxEnemies/2; i++) {
+        for (int i = 0; i < MaxEnemies / 2; i++) {
             Enemies.AddEntity(new Enemy(
-                new DynamicShape(new Vec2F(0.55f + (float)i * 0.1f, 0.9f), new Vec2F(0.1f, 0.1f)),
+                new DynamicShape(new Vec2F(0.55f + (float) i * 0.1f, 0.9f), new Vec2F(0.1f, 0.1f)),
                 blueMonster));
         }
     }
