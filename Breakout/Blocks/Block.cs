73c35e24f68f7f9b7328998226fc8e7a15222a34
student <bogi@protonmail.ch>
2023-04-23 16:49:12 +0200

    Added a boolean property and conditionals to blocks that checks whether a block is unbreakable

--- Breakout/Blocks/Block.cs
+++ Breakout/Blocks/Block.cs
@@ -7,8 +7,13 @@ namespace Breakout.Blocks;
 public class Block : Entity {
     private int value;
     public int Value {
-        get { return Value; }
+        get { return value; }
         set { Value = value; }
+    }
+    private bool isUnbreakable;
+    public bool IsUnbreakable {
+        get { return isUnbreakable; }
+        set { isUnbreakable = value; }
     }
     private int health;
     private Vec2F position;
@@ -16,10 +21,12 @@ public class Block : Entity {
         position = new Vec2F(shape.Position.X, shape.Position.Y);
     }
     public void LoseHealth() {
-        health -= 1;
+        if (isUnbreakable == false) {
+            health -= 1;
+        }
     }
     public bool IsDead() {
-        if (health <= 0) {
+        if (health <= 0 && isUnbreakable == false) {
             return true;
         } else {
             return false;   
