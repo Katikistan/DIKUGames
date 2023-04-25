283dfccf79367fdf2b4e8b80b39c2043ad8d7200
Katikistan <112586661+Katikistan@users.noreply.github.com>
2023-04-25 13:28:54 +0200

    changes to block

--- Breakout/Blocks/Block.cs
+++ Breakout/Blocks/Block.cs
@@ -10,31 +10,37 @@ public class Block : Entity {
         get { return value; }
         set { Value = value; }
     }
-    private bool isUnbreakable;
-    public bool IsUnbreakable {
-        get { return isUnbreakable; }
-        set { isUnbreakable = value; }
+    private bool unbreakable;
+    public bool Unbreakable {
+        get { return unbreakable; }
+        set { unbreakable = value; }
     }
-    private int health;
+    private bool hardned;
+    public bool Hardned {
+        get { return hardned; }
+        set { hardned = value; }
+    }
+    private int health = 1;
     private Vec2F position;
-    private DynamicShape shape;
     public Block(DynamicShape shape, IBaseImage image) : base(shape, image) {
         position = new Vec2F(shape.Position.X, shape.Position.Y);
-        this.shape = shape;
+    }
+    public void applyMeta() {
     }
     public void LoseHealth() {
-        if (isUnbreakable == false) {
+        if (!Unbreakable) {
             health -= 1;
         }
     }
     public bool IsDead() {
-        if (health <= 0 && isUnbreakable == false) {
+        if (health <= 0 && !Unbreakable) {
             return true;
         } else {
-            return false;   
+            return false;
         }
     }
     public void Render() {
         RenderEntity();
     }
 }
+
