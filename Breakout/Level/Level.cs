edfeb54a2e279a8cf68190d35100c7187d2eadf1
Katikistan <112586661+Katikistan@users.noreply.github.com>
2023-04-27 00:10:50 +0200

    changes to level and levelLoader

--- Breakout/Level/Level.cs
+++ Breakout/Level/Level.cs
@@ -1,56 +1,45 @@
 using Breakout.Blocks;
 using DIKUArcade.Entities;
-using System;
-using System.IO;
-using System.Collections.Generic;
 using DIKUArcade.Graphics;
 using DIKUArcade.Math;
-using DIKUArcade.Entities;
 namespace Breakout.Levels;
 
 public class Level {
-    public char[][] ?Map;
-
-    public Dictionary<string,string> ?Meta;
-
-    public Dictionary<char,string> ?Legend;
-
-    public LevelLoader levelLoader;
+    private char[][] ?Map;
+    private Dictionary<string,string> ?Meta;
+    private Dictionary<char,string> Legend;
+    private LevelLoader levelLoader;
     public EntityContainer<Block> blocks;
-    public Level (string startlevel) {
+    public Level () {
         this.levelLoader = new LevelLoader(Path.Combine("..","Breakout","Assets","Levels"));
-        this.levelLoader.ReadLevel(startlevel);
+        this.blocks = new EntityContainer<Block>(0);
+    }
+    public void NewLevel(string level) {
+        //next map if level input is not valid
+        levelLoader.ReadLevel(level);
         this.Map = levelLoader.Map;
         this.Meta = levelLoader.Meta;
         this.Legend = levelLoader.Legend;
+        CreateBlocks();
     }
-
-    public void NewLevel(string nextlevel) {
-        levelLoader.ReadLevel(nextlevel);
-        this.Map = levelLoader.Map;
-        this.Meta = levelLoader.Meta;
-        this.Legend = levelLoader.Legend;
-    }
-    public void DrawMap() {
-       if (Map != null) {
+    private void CreateBlocks() {
+       if (Map != null && Legend != null) {
             blocks = new EntityContainer<Breakout.Blocks.Block>(324);
-            var block_extentX = 1f/(float)Map[0].Length;
-            var block_extentY = block_extentX/3f;
+            float block_extentX = 1f/(float)Map[0].Length;
+            float block_extentY = block_extentX/3f;
             for (int i = 0; i < Map.Length; i++) {
                 for (int j = 0; j < Map[i].Length; j++) {
                     string colour = "";
-
                     Legend.TryGetValue(Map[i][j], out colour);
-
                     if (Map[i][j] != '-') {
-
-                        blocks.AddEntity(new Breakout.Blocks.Block(
-                            new StationaryShape(new Vec2F((block_extentX * (float)j), 1.0f - (block_extentY * (float)i)), new Vec2F(block_extentX, block_extentY)),
+                        blocks.AddEntity(new DefaultBlock(
+                            new StationaryShape(
+                            new Vec2F((block_extentX * (float)j), 1.0f - (block_extentY * (float)i)), new Vec2F(block_extentX, block_extentY)),
                             new Image(Path.Combine("..","Breakout","Assets", "Images", colour))
                             ));
-                        }
                     }
                 }
+            }
        }
     }
 }
--- Breakout/Level/LevelLoader.cs
+++ Breakout/Level/LevelLoader.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 
 namespace Breakout.Levels;
 public class LevelLoader {
-    private string txtfile;
+    private string txtfile = null!;
     private string path;
-    private  string[] txtlines;
+    private  string[] txtlines = null!;
     public Dictionary<string,string> ?Meta = null;
     public Dictionary<char,string> ?Legend = null;
     public char[][] ?Map = null;
@@ -27,8 +27,8 @@ public class LevelLoader {
         }
     }
     private void ReadMap() {
-        //tjekker om der er et map i filen
-        if (Array.IndexOf(txtlines, "Map:") == -1){
+        // skal tjekke map filen ikke har en linje der er for lang eller bred
+        if (Array.IndexOf(txtlines, "Map:") == -1 || Array.IndexOf(txtlines, "Map/") == -1){
             Map = null;
         } else {
             int MapStart = Array.IndexOf(txtlines, "Map:");
@@ -41,38 +41,37 @@ public class LevelLoader {
     }
     private void ReadMeta(){
         //tjekker om der er Meta i filen
-        if (Array.IndexOf(txtlines, "Meta:") == -1){
-            throw new ArgumentException("Error: no Meta found in level file");
-        }
-        int MetaStart = Array.IndexOf(txtlines, "Meta:");
-        int MetaEnd = Array.IndexOf(txtlines, "Meta/");
-        Meta = new Dictionary<string, string>();
-        for (int i = MetaStart + 1; i < MetaEnd; i++)
-        { // ikke rigtigt det skal ind i en dictonary
-            string line = txtlines[i];
-            string[] parts = line.Split(": ");
-            string key = parts[0];
-            string value = parts[1];
-            Meta[key] = value;
-            if (key == "PowerUp" || key == "Unbreakable" || key == "Hardened") {
-                Meta[value] = key;
-            } else {
-                Meta[key] = value;
+        if (Array.IndexOf(txtlines, "Meta:") == -1 || Array.IndexOf(txtlines, "Meta/") == -1){
+            Meta = null;
+        } else {
+            int MetaStart = Array.IndexOf(txtlines, "Meta:");
+            int MetaEnd = Array.IndexOf(txtlines, "Meta/");
+            Meta = new Dictionary<string, string>();
+            for (int i = MetaStart + 1; i < MetaEnd; i++) {
+                string[] parts = txtlines[i].Split(": ");
+                string key = parts[0];
+                string value = parts[1];
+                if (value.Length == 1) {
+                    Meta[value] = key;
+                } else {
+                    Meta[key] = value;
+                }
             }
         }
     }
     private void ReadLegend() {
         //tjekker om der er Legend i filen
-        if (Array.IndexOf(txtlines, "Legend:") == -1){
-            throw new ArgumentException("Error: no Legend found in level file");
-        }
-        int legendStart = Array.IndexOf(txtlines, "Legend:");
-        int legendEnd = Array.IndexOf(txtlines, "Legend/");
-        Legend = new Dictionary<char, string>();
-        for (int i = legendStart+1; i < legendEnd; i++){
-            char symbol = txtlines[i][0];
-            string imagefile = txtlines[i].Substring(3);
-            Legend[symbol] = imagefile;
+        if (Array.IndexOf(txtlines, "Legend:") == -1 || Array.IndexOf(txtlines, "Legend/") == -1){
+            Legend = null;
+        } else {
+            int legendStart = Array.IndexOf(txtlines, "Legend:");
+            int legendEnd = Array.IndexOf(txtlines, "Legend/");
+            Legend = new Dictionary<char, string>();
+            for (int i = legendStart+1; i < legendEnd; i++){
+                char symbol = txtlines[i][0];
+                string imagefile = txtlines[i].Substring(3);
+                Legend[symbol] = imagefile;
+            }
         }
     }
 }
