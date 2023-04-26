c0e7217d7b9d1d5bd855f2b142e4d2cbac9eb88e
Katikistan <112586661+Katikistan@users.noreply.github.com>
2023-04-27 00:09:47 +0200

    made block abstract and created default block type

--- Breakout/Blocks/Block.cs
+++ Breakout/Blocks/Block.cs
@@ -1,47 +1,21 @@
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
 using DIKUArcade.Math;
-
 namespace Breakout.Blocks;
+public abstract class Block : Entity {
+    internal int value = 100;
+    public int Value { get { return value; } }
+    internal int health = 1;
+    public int Health { get { return health; } }
+    internal Vec2F position; // why safe the pos
 
-public class Block : Entity {
-    private int value;
-    public int Value {
-        get { return value; }
-        set { Value = value; }
-    }
-    private bool unbreakable;
-    public bool Unbreakable {
-        get { return unbreakable; }
-        set { unbreakable = value; }
-    }
-    private bool hardned;
-    public bool Hardned {
-        get { return hardned; }
-        set { hardned = value; }
-    }
-    private int health = 1;
-    private Vec2F position;
-    private StationaryShape shape;
     public Block(StationaryShape shape, IBaseImage image) : base(shape, image) {
         position = new Vec2F(shape.Position.X, shape.Position.Y);
     }
-    public void applyMeta() {
-    }
     public void LoseHealth() {
-        if (!Unbreakable) {
-            health -= 1;
-        }
-        if (IsDead()) {
-            DeleteEntity();
-        }
-
-    }
-    public bool IsDead() {
+        health -= 1;
         if (health <= 0) {
-            return true;
-        } else {
-            return false;
+            DeleteEntity();
         }
     }
     public void Render() {
---
+++ Breakout/Blocks/DefaultBlock.cs
@@ -0,0 +1,8 @@
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+namespace Breakout.Blocks;
+public class DefaultBlock : Block {
+    public DefaultBlock(StationaryShape shape, IBaseImage image) : base(shape, image) {
+    }
+}
