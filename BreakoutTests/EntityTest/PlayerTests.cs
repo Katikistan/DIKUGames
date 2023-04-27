6b253e7bf276820128d691970fc636e75741b893
student <olifiurlovic@gmail.com>
2023-04-28 00:26:11 +0200

    comments to test

--- BreakoutTests/EntityTest/PlayerTests.cs
+++ BreakoutTests/EntityTest/PlayerTests.cs
@@ -50,10 +50,9 @@ public class TestPlayer {
         });
     }
     [Test]
-    public void TestGetPosition() {
+    public void TestPlayerIsCentered() {
         playerPos = player.GetPosition();
-        Assert.That((playerPos.X), Is.EqualTo(playerPos.X));
-        Assert.That((playerPos.Y), Is.EqualTo(playerPos.Y));
+        Assert.That(playerPos.X+ (player.Shape.Extent.X / 2f), Is.EqualTo(0.5));
     }
     [Test]
     public void TestMoveLeft() {
@@ -77,10 +76,13 @@ public class TestPlayer {
         TesteventBus.ProcessEventsSequentially();
         player.Move();
         playerPos = player.GetPosition();
+        // Player moved to the left
         Assert.That(playerPos.X, Is.EqualTo(startPosX - movementSpeed));
+        // Left key is released
         TesteventBus.RegisterEvent(eventRealeseLeft);
         TesteventBus.ProcessEventsSequentially();
         player.Move();
+        // Player pos is the same/hasn't moved
         Assert.That(playerPos.X, Is.EqualTo(startPosX - movementSpeed));
     }
     [Test]
@@ -88,10 +90,15 @@ public class TestPlayer {
         TesteventBus.RegisterEvent(eventMoveRight);
         TesteventBus.ProcessEventsSequentially();
         player.Move();
+        playerPos = player.GetPosition();
+        // Player moved to the right
+        Assert.That(playerPos.X, Is.EqualTo(startPosX + movementSpeed));
+        // Right key is released
         TesteventBus.RegisterEvent(eventRealeseRight);
         TesteventBus.ProcessEventsSequentially();
         player.Move();
         playerPos = player.GetPosition();
+        // Player pos is the same/hasn't moved
         Assert.That(playerPos.X, Is.EqualTo(startPosX + movementSpeed));
     }
     [Test]
@@ -118,13 +125,14 @@ public class TestPlayer {
         player.Move();
         player.Move();
         playerPos = player.GetPosition();
-        Assert.That(playerPos.X, Is.LessThan(1.0f));
+        Assert.That(playerPos.X + player.Shape.Extent.X, Is.LessThanOrEqualTo(1.0f));
     }
     [Test]
     public void TestMoveLeftRight() {
         TesteventBus.RegisterEvent(eventMoveLeft);
         TesteventBus.RegisterEvent(eventMoveRight);
         TesteventBus.ProcessEventsSequentially();
+        // Both left and right "keys" are pressed
         player.Move();
         player.Move();
         playerPos = player.GetPosition();
@@ -134,14 +142,18 @@ public class TestPlayer {
     public void TestMoveLeftRight2() {
         TesteventBus.RegisterEvent(eventMoveLeft);
         TesteventBus.ProcessEventsSequentially();
+        // Player moves to the left
         player.Move();
         playerPos = player.GetPosition();
         Assert.That(playerPos.X, Is.EqualTo(startPosX - movementSpeed));
+        // Player realeses left key
         TesteventBus.RegisterEvent(eventRealeseLeft);
+        // Player presses right key
         TesteventBus.RegisterEvent(eventMoveRight);
         TesteventBus.ProcessEventsSequentially();
         player.Move();
         playerPos = player.GetPosition();
+        // Player moved left and right back to start
         Assert.That(playerPos.X, Is.EqualTo(startPosX));
     }
     [Test]
--- BreakoutTests/LevelLoadingTests/LevelTests.cs
+++ BreakoutTests/LevelLoadingTests/LevelTests.cs
@@ -1,18 +1,17 @@
-// using Breakout.Levels;
-// namespace BreakoutTests.LevelLoading;
-// [TestFixture]
-// public class LevelTests {
-//     private Level level = null!;
-//     public LevelTests() {
-//     }
-//     [SetUp]
-//     public void Setup() {
-//         level = new Level();
-//     }
-// // test man kan loade et nyt level
-//     [Test]
-//     public void TestNewLevel() {
-//         Assert.That(level.NewLevel("level1.txt"), Is.True);
-//     }
-// }
+using Breakout.Levels;
+namespace BreakoutTests.LevelLoading;
+[TestFixture]
+public class LevelTests {
+    private Level level = null!;
+    public LevelTests() {
+    }
+    [SetUp]
+    public void Setup() {
+        level = new Level();
+    }
+    [Test]
+    public void TestNewLevel() {
+        // Assert.That(level.NewLevel("level2.txt"), Is.True);
+    }
+}
 
