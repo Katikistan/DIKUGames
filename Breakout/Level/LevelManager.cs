80aa42583f94302eeadb057240f879ad4ba266db
ThomRP <thomrisped@gmail.com>
2023-05-04 16:17:00 +0200

    WallCollisions so ball bounces off walls + levelmanager with ball

--- Breakout/Level/LevelManager.cs
+++ Breakout/Level/LevelManager.cs
@@ -13,12 +13,14 @@ public class LevelManager {
     public EntityContainer<Block> blocks;
     private Player player;
     private EntityContainer<Ball> balls;
+    private Ball ball;
     public LevelManager() {
         levelCreator = new LevelCreator();
         player = new Player(
             new DynamicShape(new Vec2F(0.425f, 0.06f), new Vec2F(0.15f, 0.04f)),
             new Image(Path.Combine("..", "Breakout", "Assets", "Images", "player.png")));
-        // balls = levelCreator.Balls;
+        ball = new Ball(new DynamicShape(new Vec2F(0.45f, 0.2f), new Vec2F(0.03f, 0.03f), new Vec2F(0.0f,0.008f)), 
+        new Image(Path.Combine("..", "Breakout", "Assets", "Images", "ball2.png")));
     }
     public void NewLevel(string level) {
         levelCreator.CreateLevel(level);
@@ -27,14 +29,18 @@ public class LevelManager {
     private void CheckCollisions() {
         PlayerCollsion.Collide(balls, player);
         BlockCollsion.Collide(balls, blocks);
-        WallCollsion.Collide(balls);
+        WallCollsion.Collide(ball);
+
+
     }
     public void Render() {
         player.Render();
         blocks.RenderEntities();
+        ball.Render();
     }
     public void Update() {
         CheckCollisions();
         player.Move();
+        ball.Move();
     }
 }
