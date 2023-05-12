110773494628107eb32405d6bd302a7b064e09ea
Katikistan <112586661+Katikistan@users.noreply.github.com>
2023-05-12 10:06:13 +0200

    used for testing

---
+++ Breakout/Constants.cs
@@ -0,0 +1,12 @@
+using DIKUArcade.Utilities;
+using System.IO;
+namespace Breakout;
+
+/// <summary> Class for conatining constants </summary>
+/// <remarks> Used for indicating path to assets </remarks>
+public static class Constants {
+    /// <summary> This is to get the path of the Breakout/Assets </summary>
+    /// <remarks> We need this for testing, to make sure we are using the same assets </remarks>
+    public static readonly string MAIN_PATH =
+        Path.Combine(Directory.GetParent(FileIO.GetProjectPath())!.FullName,"Breakout");
+}
