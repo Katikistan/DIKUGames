7614bcebe83a0757a867ce52c5880f401354a58c
student <bogi@protonmail.ch>
2023-05-24 16:26:39 +0200

    Addd powerupcollision tests

---
+++ BreakoutTests/CollisionsTests/PowerUpCollisionTest.cs
@@ -0,0 +1,37 @@
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+using Breakout.Balls;
+using Breakout.Levels;
+using Breakout.Players;
+using Breakout.Collisions;
+using Breakout.Powerups;
+namespace BreakoutTests.CollisionTests;
+
+[TestFixture]
+public class PowerUpCollisionTest {
+    private Player player;
+    private Powerup powerup1;
+    private Powerup powerup2;
+    private EntityContainer<Powerup> powerups;
+    public PowerUpCollisionTest() {
+        DIKUArcade.GUI.Window.CreateOpenGLContext();
+    }
+    [SetUp]
+    public void Setup(){
+        powerup1 = PowerUpCreator.CreatePowerUp(new Vec2F(0.425f, 0.06f));
+        powerup2 = PowerUpCreator.CreatePowerUp(new Vec2F(0.5f, 0.5f));
+        powerups = new EntityContainer<Powerup>(2);
+        powerups.AddEntity(powerup1);
+        powerups.AddEntity(powerup2);
+        player = new Player(
+            new DynamicShape(new Vec2F(0.425f, 0.06f), new Vec2F(0.15f, 0.04f)),
+            new Image(Path.Combine("..", "Breakout", "Assets", "Images", "player.png")));
+    }
+    [Test]
+    public void TestCollision() {
+        Assert.AreEqual(powerups.CountEntities(), 2);
+        PowerUpCollision.Collide(powerups, player);
+        Assert.AreEqual(powerups.CountEntities(), 1);
+    }
+}
