b26baf40b8c5d0b210159a0282da6810a0d8fd51
Katikistan <112586661+Katikistan@users.noreply.github.com>
2023-06-03 18:43:43 +0200

    game state test fix

--- BreakoutTests/StatesTest/GameRunningTests.cs
+++ BreakoutTests/StatesTest/GameRunningTests.cs
@@ -22,22 +22,22 @@ public class GameRunningTest {
 
         Assert.That(gameRunning.Health is Health);
         Assert.That(gameRunning.Background is Entity);
-        Assert.That(gameRunning.Levels is List<string>);
+        Assert.That(gameRunning.Levellst is List<string>);
         Assert.That(gameRunning.LevelManager is LevelManager);
         Assert.That(gameRunning.Points is Points);
 
-        Assert.AreEqual(gameRunning.Levels[0], "level1.txt");
-        Assert.AreEqual(gameRunning.Levels[1], "level2.txt");
-        Assert.AreEqual(gameRunning.Levels[2], "level3.txt");
+        Assert.AreEqual(gameRunning.Levellst[0], "level1.txt");
+        Assert.AreEqual(gameRunning.Levellst[1], "level2.txt");
+        Assert.AreEqual(gameRunning.Levellst[2], "level3.txt");
     }
     [Test]
     public void TestLoadLevels() {
         // If levels is empty a new level is created
-        Assert.AreEqual(5,gameRunning.Levels.Count);
-        for (int i = 0; i < gameRunning.Levels.Count; i++) {
-            gameRunning.Levels.RemoveAt(i);
+        Assert.AreEqual(5,gameRunning.Levellst.Count);
+        for (int i = 0; i < gameRunning.Levellst.Count; i++) {
+            gameRunning.Levellst.RemoveAt(i);
         }
         gameRunning.UpdateState();
-        Assert.AreEqual(gameRunning.Levels.Count, 2);
+        Assert.AreEqual(gameRunning.Levellst.Count, 2);
     }
 }
