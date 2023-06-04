91058ffd001aac0b1aca30a0e4759d3bdd3fe8ed
Katikistan <112586661+Katikistan@users.noreply.github.com>
2023-06-04 18:14:13 +0200

    added test for powerupcreator

---
+++ BreakoutTests/UnitTests/PowerupTests/PowerupCreatorTests.cs
@@ -0,0 +1,29 @@
+using Breakout;
+using Breakout.Collisions;
+using Breakout.Players;
+using Breakout.Powerups;
+using DIKUArcade.Entities;
+using DIKUArcade.Events;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+namespace BreakoutTests.PowerupTests;
+[TestFixture]
+public class PowerupCreatorTests {
+    public Powerup lifeplus;
+    public Health health;
+    public EntityContainer<Powerup> powerups;
+    public Player player;
+
+    public PowerupCreatorTests() {
+        DIKUArcade.GUI.Window.CreateOpenGLContext();
+    }
+        [Test(), Repeat(20)]
+        public void TestCreatePowerUp() {
+            // Arrange
+            Vec2F pos = new Vec2F(0.5f, 0.5f);
+            Powerup powerup = PowerUpCreator.CreatePowerUp(pos);
+            Assert.That(powerup is not null);
+            Assert.That(powerup is Powerup);
+        }
+}
+
