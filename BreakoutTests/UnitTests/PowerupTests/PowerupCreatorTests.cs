bf82cb46a4c29ee322ff7e24d21f48eb52c9dfb6
Katikistan <112586661+Katikistan@users.noreply.github.com>
2023-06-04 18:31:44 +0200

    haha

--- BreakoutTests/UnitTests/PowerupTests/PowerupCreatorTests.cs
+++ BreakoutTests/UnitTests/PowerupTests/PowerupCreatorTests.cs
@@ -19,7 +19,6 @@ public class PowerupCreatorTests {
     }
         [Test(), Repeat(30)]
         public void TestCreatePowerUp() {
-            // Arrange
             Vec2F pos = new Vec2F(0.5f, 0.5f);
             Powerup powerup = PowerUpCreator.CreatePowerUp(pos);
             Assert.That(powerup is not null);
