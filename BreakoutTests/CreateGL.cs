8162b9b306398c17225120939afc24049ee556d8
Katikistan <112586661+Katikistan@users.noreply.github.com>
2023-06-06 09:19:19 +0200

    xml

--- BreakoutTests/CreateGL.cs
+++ BreakoutTests/CreateGL.cs
@@ -1,4 +1,7 @@
 namespace BreakoutTests;
+/// <summary>
+/// A class used in testing to avoid 50+ windows opening and crashing tests
+/// </summary>
 public static class CreateGL {
     private static bool created = false;
 
